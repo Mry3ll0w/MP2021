7de59581e0cd28d39e9baaa566309d01a31fd95f
Antonio Roldan Andrade <antonioroldanandrade@gmail.com>
2021-02-23 19:52:39 +0100

    He subido una mini estructura con los txt que sirven de base de datos y unas cabeceras de la funcion core que voy a implementar mñn

---
+++ lib/core.h
@@ -0,0 +1,16 @@
+//
+// Created by anton on 23/02/2021.
+//
+
+#ifndef MP2021_CORE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+//Libs del sistema necesarias para el resto de la ejecucion
+
+void generic_reader();//Implementar lo primero
+void single_loader();//Guarda un único elemento en su struct corresppndiente
+
+#define MP2021_CORE_H
+
+#endif //MP2021_CORE_H
---
+++ lib/users.h
@@ -0,0 +1,8 @@
+//
+// Created by anton on 23/02/2021.
+//
+
+#ifndef MP2021_USERS_H
+#define MP2021_USERS_H
+
+#endif //MP2021_USERS_H
