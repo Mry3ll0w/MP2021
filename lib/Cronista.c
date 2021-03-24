832d345b172a8a28893d5a658a71bfddcaabb2d3
Marcos <marcos.perezfernandez@alum.uca.es>
2021-03-24 20:22:05 +0100

    Compila perfe cronista
    Faltaría arreglar la función de valoraciones de plantillas y ya está

--- lib/Cronista.c
+++ lib/Cronista.c
@@ -32,10 +32,8 @@ void cro_menu()
         fflush(stdin);
 
         switch (opcion) {
-            case 1:{system("cls");
-                cro_listar_equipos(); }break;
-            case 2:{ system("cls");
-                cro_valorar_equipos(); }break;
+            case 1:  cro_listar_equipos();break;
+            case 2:  cro_valorar_equipos();break;
 
         }
     } while(opcion!=3);
@@ -51,7 +49,7 @@ void cro_listar_equipos()
 
     if(configuration.max_teams!=0) {
         for (i = 0; i < configuration.max_teams; ++i) {
-
+            fflush(stdin);
             printf("\t %i . %s\n",i, equipos[i].nombre);
         }
     }
@@ -60,37 +58,42 @@ void cro_listar_equipos()
         printf("\t No exixte ningun equipo aun.\n");
 
     }
+    system("pause");
+    system("cls");
 
 }
 
 void cro_valorar_equipos()
 {
-int i,j,opc;
-char actual_team_id[3];
-do{
-printf("\t Seleccione el equipo de la plantilla que quiera valorar: \n");
-scanf("%i",&i);
-system("pause");
-system("cls");
-printf("\t %s \n",equipos[i].nombre);
-fflush(stdin);
-strcpy(actual_team_id , equipos[i].id );
+    int i,j,opc;
+    char actual_team_id[3];
+    do{
+        printf("\t Seleccione el equipo de la plantilla que quiera valorar: \n");
+        scanf("%i",&i);
+        fflush(stdin);
+        system("pause");
+        system("cls");
+        printf("\t %s \n",equipos[i].nombre);
+        fflush(stdin);
+        strcpy(actual_team_id , equipos[i].id );
 
 
-for (j = 0; j < configuration.maxplayersperteam; ++j) {
-if(strcmp(actual_team_id,jugadores[j].id)==0)
-{
-printf("\t Nueva valoracion del jugador %s: \n", jugadores[j].nombre);
-scanf("%i", jugadores[j].valoracion);
-Core_football_players_update();
-}
-}
-printf("\t 1. Valorar otra plantilla. \n");
-printf("\t 2. Volver al menu cronista. \n");
-scanf("%i",&opc);
-system("cls");
-}while(opc!=2);
-cro_menu();
+        for (j = 0; j < configuration.maxplayersperteam; ++j) {
+            if(strcmp(actual_team_id,jugadores[j].id)==0)
+        {
+        printf("\t Nueva valoracion del jugador %s: \n", jugadores[j].nombre);
+        scanf("%i", jugadores[j].valoracion);
+        Core_football_players_update();
+        }
+        }
+        printf("\t 1. Valorar otra plantilla. \n");
+        printf("\t 2. Volver al menu cronista. \n");
+        scanf("%i",&opc);
+        system("cls");
+    }while(opc!=2);
+    system("pause");
+    system("cls");
+    cro_menu();
 
 }
 
