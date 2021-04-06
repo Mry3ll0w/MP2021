4716d2c54b86688a71f326ec5cc84c15fda65003
Mry3ll0w <48240649+mry3ll0w@users.noreply.github.com>
2021-04-06 17:47:41 +0200

    fix valorar equipos

--- lib/Cronista.c
+++ lib/Cronista.c
@@ -6,7 +6,7 @@
 #include "core.h"
 
 void cro_menu();
-void cro_listar_equipos();
+void cro_listar_equipos();//
 void cro_valorar_equipos();
 
 void cro_menu()
@@ -53,7 +53,6 @@ void cro_listar_equipos()
         printf("\t %i . %s\n",i, equipos[i].nombre);
 
     }
-
     system("pause");
     system("cls");
 
@@ -61,36 +60,31 @@ void cro_listar_equipos()
 
 void cro_valorar_equipos()
 {
-    int i,j,opc;
+    int j,opc;
     char actual_team_id[3];
     do{
-        printf("\t Seleccione el equipo de la plantilla que quiera valorar: \n");
-        scanf("%i",&i);
-        fflush(stdin);
-        system("pause");
-        system("cls");
-        printf("\t %s \n",equipos[i].nombre);
-        fflush(stdin);
-        strcpy(actual_team_id , equipos[i].id );
-
-
-        for (j = 0; j < configuration.maxplayersperteam; ++j) {
-            if(strcmp(actual_team_id,jugadores[j].id)==0)
-        {
-        printf("\t Nueva valoracion del jugador %s: \n", jugadores[j].nombre);
-        scanf("%i", jugadores[j].valoracion);
-        Core_football_players_update();
+        printf("\t Seleccione el  ID del equipo de la plantilla que quiera valorar: \n");
+        scanf("%s",actual_team_id);
+        //system("pause");
+        //system("cls");
+        for (j = 0; j < configuration.planter_counter; ++j) {
+            if(strcmp(actual_team_id,equipos[j].id)==0)
+            {
+                break;
+            }
         }
+        for (int i = 0; i <equipos[j].assigned_players ; ++i) {
+            printf("Jugadores asignados: %i\n",equipos[j].assigned_players);
+                printf("\t Nueva valoracion del jugador %s: \n", jugadores[i].nombre);
+                scanf("%i", &jugadores[i].valoracion);
         }
         printf("\t 1. Valorar otra plantilla. \n");
         printf("\t 2. Volver al menu cronista. \n");
         scanf("%i",&opc);
         system("cls");
     }while(opc!=2);
-    system("pause");
-    system("cls");
+    Core_football_players_update();
     cro_menu();
-
 }
 
 
